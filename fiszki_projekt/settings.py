7dc2df072f1f6ff1fac94d4263a81c7e953f1d9f
InTheShadow <tomkopc9@gmail.com>
2025-11-11 12:43:16 +0100

    Added static directory

--- fiszki_projekt/settings.py
+++ fiszki_projekt/settings.py
@@ -121,6 +121,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
